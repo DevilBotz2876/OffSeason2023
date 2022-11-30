8f43529ccc71e87bdce90cbcd94cefeb7d11574a
Parker Meyers <parker@parkermeyers.com>
2022-11-30 18:43:39 -0500

    Add Drive Train

---
+++ src/main/java/bhs/devilbotz/commands/DriveCommand.java
@@ -0,0 +1,62 @@
+/*-------------------------------------------------------------------------------*/
+/* Copyright (c) 2022-2023 BHS Devilbotz. All Rights Reserved.                   */
+/* Open Source Software - may be modified, commercialized, distributed,          */
+/* sub-licensed and used for private use under the terms of the License.md       */
+/* file in the root of the source code tree.                                     */
+/*                                                                               */
+/* You MUST include the original copyright and license files in any and all      */
+/* revised/modified code. You may NOT remove this header under any circumstance  */
+/* unless explicitly noted                                                       */
+/*-------------------------------------------------------------------------------*/
+
+package bhs.devilbotz.commands;
+
+import bhs.devilbotz.subsystems.DriveTrain;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+import java.util.function.DoubleSupplier;
+
+public class DriveCommand extends CommandBase {
+    private final DriveTrain drive;
+    private final DoubleSupplier left;
+    private final DoubleSupplier right;
+
+    public DriveCommand(DriveTrain drive, DoubleSupplier left, DoubleSupplier right) {
+        this.drive = drive;
+        this.left = left;
+        this.right = right;
+        addRequirements(this.drive);
+    }
+
+    @Override
+    public void execute() {
+        double r = right.getAsDouble();
+        double l = left.getAsDouble();
+
+        // Forward Snapping
+        if ((Math.abs((l - r) / r) < 0.15 || Math.abs((r - l) / l) < 0.15) && ((l > 0.05 || l < -0.05) || (r > 0.05 || r < -0.05))) {
+            double oldLeft = l;
+            double oldRight = l;
+
+            l = (oldLeft + oldRight) / 2;
+
+            r = (oldLeft + oldRight) / 2;
+        }
+
+        // (a*(x^3)+(b-a)*x)*1.1
+        double a = 0.2;
+        double b = 0.9;
+
+        // Modified curve
+        r = (a * (r * r * r) + (b - a) * r) * 1.1;
+        l = (a * (l * l * l) + (b - a) * l) * 1.1;
+
+        drive.tankDrive(l, r);
+    }
+
+    @Override
+    public boolean isFinished() {
+        return false;
+    }
+
+}
---
+++ src/main/java/bhs/devilbotz/subsystems/DriveTrain.java
@@ -0,0 +1,87 @@
+// Copyright (c) FIRST and other WPILib contributors.
+
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package bhs.devilbotz.subsystems;
+
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import com.ctre.phoenix.motorcontrol.InvertType;
+
+/**
+ * Creates a new DriveTrain.
+ *
+ * @author Sowndy J, Alex C, Parker M, Krish S
+ * @since 1.0.0
+ */
+public class DriveTrain extends SubsystemBase {
+
+    // Define talons
+    private static final WPI_TalonSRX leftMaster = new WPI_TalonSRX(3);
+    private static final WPI_TalonSRX rightMaster = new WPI_TalonSRX(1);
+    private static final WPI_TalonSRX leftFollower = new WPI_TalonSRX(4);
+    private static final WPI_TalonSRX rightFollower = new WPI_TalonSRX(2);
+
+    //Follower - Master Talon is "given" the instructions and the follower talons controlling the other motors essentially follow the master talons
+
+
+    //Define differential drive
+    private final DifferentialDrive differentialDrive = new DifferentialDrive(rightMaster, leftMaster);
+
+
+
+    public DriveTrain() {
+        setupTalons();
+    }
+
+
+    @Override
+    public void periodic() {
+        // This method will be called once per scheduler run
+
+
+    }
+
+
+    @Override
+    public void simulationPeriodic() {
+        // This method will be called once per scheduler run during simulation
+
+
+    }
+
+    private void setupTalons() {
+        rightFollower.setInverted(InvertType.FollowMaster);
+        rightMaster.setInverted(false);
+        leftFollower.setInverted(InvertType.FollowMaster);
+        leftMaster.setInverted(true);
+
+        rightFollower.follow(rightMaster);
+        leftFollower.follow(leftMaster);
+    }
+
+    /**
+     * Tank drive method
+     *
+     * @param leftSpeed The speed of the left side of the robot
+     * @param rightSpeed The speed of the right side of the robot
+     * @since 1.0.0
+     */
+    public void tankDrive(double leftSpeed, double rightSpeed) {
+        differentialDrive.tankDrive(leftSpeed, rightSpeed);
+    }
+
+    public WPI_TalonSRX getLeftFollower() {
+        return leftFollower;
+    }
+
+    public WPI_TalonSRX getRightFollower() {
+        return rightFollower;
+    }
+}
+
+
+
+
